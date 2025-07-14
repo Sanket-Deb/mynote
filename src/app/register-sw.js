0889718208255310dfd777ff8cb62d02797f7f2c
Sanket Deb <imsanketdeb20@gmail.com>
2025-07-14 19:51:17 +0530

    pwa install locally executed

--- src/app/register-sw.js
+++ src/app/register-sw.js
@@ -1,1 +1,25 @@
+"use client";
 
+import { useEffect } from "react";
+
+const RegisterSW = () => {
+  useEffect(() => {
+    console.log("📦 RegisterSW mounted"); // Add this
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/sw.js").then(
+          (registration) => {
+            console.log("SW registered ✅: ", registration);
+          },
+          (error) => {
+            console.error("SW registration failed ❌: ", error);
+          }
+        );
+      });
+    }
+  }, []);
+
+  return null;
+};
+
+export default RegisterSW;
