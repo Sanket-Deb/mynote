2efe424013e4cfd5a808e17a4bc730d70013ceca
Sanket Deb <imsanketdeb20@gmail.com>
2025-08-06 19:27:41 +0530

    changing firestoreHelpers to match the updated db collection

--- src/lib/firestoreHelpers.js
+++ src/lib/firestoreHelpers.js
@@ -12,43 +12,46 @@ import {
 
 // For logged-in users
 export const createUserNote = async (uid) => {
-  const docRef = await addDoc(collection(db, "users", uid, "notes"), {
+  const docRef = await addDoc(collection(db, "notes"));
+  await setDoc(docRef, {
     content: "",
     createdAt: serverTimestamp(),
     updatedAt: serverTimestamp(),
     password: "",
+    ownerUID: uid,
   });
   return docRef.id;
 };
 
 // For non-logged-in user
 export const createAnonNote = async (noteId) => {
-  const noteRef = doc(db, "anon", noteId);
+  const noteRef = doc(db, "notes", noteId);
   await setDoc(noteRef, {
     content: "",
     createdAt: serverTimestamp(),
     updatedAt: serverTimestamp(),
     password: "",
+    ownerUID: null,
   });
   return noteId;
 };
 
 //---------unsed
 // Update content (used in autosave)
-export const updateNoteContent = async (ref, content) => {
-  await updateDoc(ref, {
-    content,
-    updatedAt: serverTimestamp(),
-  });
-};
+// export const updateNoteContent = async (ref, content) => {
+//   await updateDoc(ref, {
+//     content,
+//     updatedAt: serverTimestamp(),
+//   });
+// };
 
-// Delete any note (used by dashboard delete)
-export const deleteNote = async (ref) => {
-  await deleteDoc(ref);
-};
+// // Delete any note (used by dashboard delete)
+// export const deleteNote = async (ref) => {
+//   await deleteDoc(ref);
+// };
 
-// Get full note document
-export const getNote = async (ref) => {
-  const snap = await getDoc(ref);
-  return snap.exists() ? snap.data() : null;
-};
+// // Get full note document
+// export const getNote = async (ref) => {
+//   const snap = await getDoc(ref);
+//   return snap.exists() ? snap.data() : null;
+// };
