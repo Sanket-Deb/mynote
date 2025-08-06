9a5c27c0f135ef5e8775c109c2b209b1a94d59d2
Sanket Deb <imsanketdeb20@gmail.com>
2025-08-06 19:25:50 +0530

    updating migrateAnonNOte to match new backend db collection structure

--- src/lib/migrateAnonNote.js
+++ src/lib/migrateAnonNote.js
@@ -1,32 +1,25 @@
 import { auth, db, provider } from "./firebase";
-import {
-  doc,
-  getDoc,
-  setDoc,
-  deleteDoc,
-  serverTimestamp,
-} from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { signInWithPopup } from "firebase/auth";
 
 export const loginAndMigrateAnonNote = async (noteId) => {
   const result = await signInWithPopup(auth, provider);
   const user = result.user;
 
-  const anonRef = doc(db, "anon", noteId);
-  const anonSnap = await getDoc(anonRef);
+  const noteRef = doc(db, "notes", noteId);
+  const noteSnap = await getDoc(anonRef);
 
-  if (anonSnap.exists()) {
-    const data = anonSnap.data();
-    const userRef = doc(db, "users", user.uid, "notes", noteId);
-    await setDoc(userRef, {
-      ...data,
-      updatedAt: serverTimestamp(),
-      migrateFromAnon: true,
-    });
+  if (noteSnap.exists()) {
+    const data = noteSnap.data();
 
-    await deleteDoc(anonRef);
-    return { user, migrated: true };
-  } else {
+    if (!data.ownerUID) {
+      await updateDoc(noteRef, {
+        ownerUID: user.uid,
+        migratedFromAnon: true,
+      });
+      return { user, migrated: true };
+    }
     return { user, migrated: false };
   }
+  return { user, migrated: false };
 };
